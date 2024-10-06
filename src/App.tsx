936a5a32b27c532dd04bb36fae64fbdb0be9a6d8
Michel Machado <michelhmachado@gmail.com>
2024-10-06 00:02:35 -0300

    feat: add use of context for more efficient props passing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import "./App.css";
 import Main from "./components/Main";
 import Sidebar from "./components/Sidebar";
+import { RadioProvider } from "./contexts/RadioContext";
 
 function App() {
   return (
-    <div className="flex w-full h-full">
-      <Sidebar />
-      <Main />
-    </div>
+    <RadioProvider>
+      <div className="flex w-full min-h-full">
+        <Sidebar />
+        <Main />
+      </div>
+    </RadioProvider>
   );
 }
 
