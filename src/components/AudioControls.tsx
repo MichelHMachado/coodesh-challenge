6ad5b3ed7ed8becc1e52a205a0e2c9dae196f35d
Michel Machado <michelhmachado@gmail.com>
2024-10-06 00:11:22 -0300

    feat: Add AudioControls component

---
+++ src/components/AudioControls.tsx
@@ -0,0 +1,22 @@
+import { PlayIcon, StopIcon } from "../assets/icons";
+import IconButton from "./IconButton";
+
+interface Props {
+  isLoading: boolean;
+  onClick: () => void;
+
+  isPlaying: boolean;
+}
+const AudioControls = ({ isLoading, onClick, isPlaying }: Props) => {
+  return (
+    <>
+      {isLoading ? (
+        <div className="loader" />
+      ) : (
+        <IconButton onClick={onClick} Icon={isPlaying ? StopIcon : PlayIcon} />
+      )}
+    </>
+  );
+};
+
+export default AudioControls;
