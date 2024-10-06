46ebe2ee40e54a64bd8af10c7711c7e23509cf23
Michel Machado <michelhmachado@gmail.com>
2024-10-06 00:03:57 -0300

    chore: add search and check icons

--- src/assets/icons.ts
+++ src/assets/icons.ts
@@ -4,3 +4,5 @@ export { MdEdit as EditIcon } from "react-icons/md";
 export { FaTrash as RemoveIcon } from "react-icons/fa";
 export { FaPlay as PlayIcon } from "react-icons/fa";
 export { FaStop as StopIcon } from "react-icons/fa";
+export { FaSearch as SearchIcon } from "react-icons/fa";
+export { FaCheck as CheckIcon } from "react-icons/fa";
