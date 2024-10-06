727f1a08dc04a1e9b62bd6ab4970ce951d11ebbe
Michel Machado <michelhmachado@gmail.com>
2024-10-06 00:14:36 -0300

    chore: update RadioCardList component

--- src/components/RadioCardsList.tsx
+++ src/components/RadioCardsList.tsx
@@ -1,5 +1,25 @@
-const RadioCardsList = () => {
-  return <div>RadioCardsList</div>;
+import { RadioStation } from "../types/radioStation";
+import RadioCard from "./RadioCard";
+
+interface Props {
+  stations: RadioStation[];
+}
+
+const RadioCardsList = ({ stations }: Props) => {
+  return (
+    <>
+      {stations.map((station, index) => {
+        if (station.data) {
+          return (
+            <RadioCard
+              key={station.data?.name + index.toString()}
+              station={station.data}
+            />
+          );
+        }
+      })}
+    </>
+  );
 };
 
 export default RadioCardsList;
