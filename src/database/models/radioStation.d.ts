283d95950bc3a206b6eaead277cc97b94178fb3d
Michel Machado <michelhmachado@gmail.com>
2024-10-07 01:10:13 -0300

    chore: minor improvements

--- src/database/models/radioStation.d.ts
+++ src/database/models/radioStation.d.ts
@@ -1,8 +1,14 @@
 export interface RadioStation {
   data?: RadioStation;
+  stationuuid: string;
   name: string;
   url: string;
   country: string;
   countrycode: string;
   language: string;
+}
+
+export interface RadioStationData {
+  id: string;
+  data: RadioStation[] | RadioStation;
 }
